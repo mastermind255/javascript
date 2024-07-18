f34643dbd890ce7a1f1eda341f66daebeab43cc2
Soumitra kumar hota <116539186+mastermind255@users.noreply.github.com>
2024-07-18 22:23:03 +0530

    Add files via upload
    operator in js

---
+++ opr.js
@@ -0,0 +1,79 @@
+//arthmetic operator 
+
+let a=20;
+let b=30;
+console.log(a+b);
+document.write(a+b);// it print the output in webpage.
+
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+let c = 3;
+let d = 2;
+console.log(c**d);//it is call exponential or to the power.
+
+console.log(a++);//post increment.
+console.log(a);// we write again for getting the exact value of a.
+console.log(b--);//post decrement;
+console.log(b);//we write again for getting the exact value of b.
+
+console.log(++c);//pre increment here we need not to write the syntax again for getting the exact value,here it will directly give the exact value.
+console.log(--d);//pre decrement here we need not to write the syntax again for getting the exact value,here it will directly give the exact value.
+
+// assignment operator
+
+a=10;
+console.log(a);
+a+=5;//here it is same like a+5.
+console.log(a);
+
+a-=5;//here it is same like a-5.
+console.log(a);
+
+a*=5;//here it is same like a*5.
+console.log(a);
+
+a/=5;//here it is same like a/5.
+console.log(a);
+
+a**=2;//here it is same like a**2.
+console.log(a);
+
+// relational operator 
+
+let e = 25;
+let f = 35;
+let g = "40";
+let h = 40;
+console.log(e>f);
+console.log(e<f);
+console.log(e>=f);
+console.log(e<=f);
+console.log(e!=f);
+console.log(g == h);//here it gives true not check data type 
+console.log(g === h);// here it  false because it  check the data type .
+console.log(g !== h);
+console.log(g+h);// when we add string having number and integer value then it give str+int;
+console.log(g-h);// but here it substracting the values. 
+
+// logical operator 
+
+let ram = 100;
+let sam = 200;
+
+condition1 = ram > sam;
+condition2 = ram < sam;
+
+console.log(condition1 && condition2);
+console.log(condition1 || condition2);
+console.log(!(false));
+console.log(condition1 ^ condition2);
+
+
+
+
+
+
+
+
