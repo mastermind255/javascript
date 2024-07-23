a5f9deeb3efed14d4c2df9704a43f2fb4e7b8423
Soumitra kumar hota <116539186+mastermind255@users.noreply.github.com>
2024-07-23 23:06:32 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,19 @@
+let day = prompt("enter the number of day(1-7):  ");
+
+if (day == 1){
+    document.write("sunday");
+}else if (day == 2) {
+    document.write("monday");
+}else if (day == 3) {
+    document.write("tuesday");
+}else if (day == 4) {
+    document.write("wednesday");
+}else if (day == 5) {
+    document.write("thursday");
+}else if (day == 6) {
+    document.write("friday");
+}else if (day == 7) {
+    document.write("saturday");
+}else{
+    document.write("not a day");
+}
